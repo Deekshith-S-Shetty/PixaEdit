2a39ad95b08f1bbf28b4403486b852bfdd08e61c
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-08-10 21:57:29 +0530

    Add react-form with zod

---
+++ src/components/shared/TransformationForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CustomField from "./CustomField";
+
+const TransformationForm = () => {
+  return (
+    <div>
+      TransformationForm
+      <CustomField />
+    </div>
+  );
+};
+
+export default TransformationForm;
