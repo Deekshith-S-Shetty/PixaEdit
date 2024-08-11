a3367ed28bc02096e6c8dd3ef60c3c12bedec7dc
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-08-11 21:55:27 +0530

    complete transformation/add/fill page with select component and its handler

--- src/components/shared/TransformationForm.tsx
+++ src/components/shared/TransformationForm.tsx
@@ -1,12 +1,135 @@
-import React from "react";
-import CustomField from "./CustomField";
+"use client";
 
-const TransformationForm = () => {
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+
+import CustomField from "./CustomField";
+import { TransformationFormProps } from "@/types";
+import { aspectRatioOptions, transformationTypes } from "@/constants";
+import { AspectRatioKey } from "@/lib/utils";
+import { useState } from "react";
+
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+});
+
+const TransformationForm = ({
+  action,
+  data = null,
+  userId,
+  type,
+  creditBalance,
+  config = null,
+}: TransformationFormProps) => {
+  const transformatioinType = transformationTypes[type];
+  const [image, setImage] = useState(data);
+  const [newTransformation, setNewTransformation] = useState(config);
+
+  const initialValues = {
+    title: data?.title || "",
+    aspectRatio: data?.aspectRatio || "",
+    color: data?.color || "",
+    prompt: data?.prompt || "",
+    publicId: data?.publicId || "",
+  };
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log("values", values);
+  };
+
+  const onSelectFieldChangeHandler = (
+    value: string,
+    onChange: (value: string) => void
+  ) => {
+    const imageSize = aspectRatioOptions[value as AspectRatioKey];
+
+    setImage((prevState: any) => ({
+      ...prevState,
+      aspectRatio: value,
+      width: imageSize.width,
+      height: imageSize.height,
+    }));
+
+    setNewTransformation(transformatioinType.config);
+    return onChange(value);
+  };
+
   return (
-    <div>
-      TransformationForm
-      <CustomField />
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <CustomField
+          control={form.control}
+          name="title"
+          formLabel="Image Title"
+          className="w-full"
+          render={({ field }) => {
+            return <Input {...field} className="input-field -ml-1" />;
+          }}
+        />
+      </form>
+
+      {type === "fill" && (
+        <CustomField
+          control={form.control}
+          name="aspectRatio"
+          formLabel="Aspect Ratio"
+          className="w-full"
+          render={({ field }) => {
+            return (
+              <Select
+                value={field.value}
+                onValueChange={(value) =>
+                  onSelectFieldChangeHandler(value, field.onChange)
+                }
+              >
+                <SelectTrigger className="select-field">
+                  <SelectValue placeholder="Select size">
+                    {field.value}
+                  </SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  {Object.keys(aspectRatioOptions).map((key) => {
+                    return (
+                      <SelectItem key={key} value={key} className="select-item">
+                        {aspectRatioOptions[key as AspectRatioKey].label}
+                      </SelectItem>
+                    );
+                  })}
+                </SelectContent>
+              </Select>
+            );
+          }}
+        />
+      )}
+    </Form>
   );
 };
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { aspectRatioOptions } from "@/constants";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -21,3 +22,5 @@ export const handleError = (error: unknown) => {
     throw new Error(`Unknown error: ${JSON.stringify(error)}`);
   }
 };
+
+export type AspectRatioKey = keyof typeof aspectRatioOptions;
