6c8cb8f3839fa4d646ac39bba32caa73f35a587b
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-08-25 20:47:45 +0530

    Add alert for when user runs out of credits

--- src/components/shared/TransformationForm.tsx
+++ src/components/shared/TransformationForm.tsx
@@ -39,6 +39,7 @@ import { getCldImageUrl } from "next-cloudinary";
 import { addImage, updateImage } from "@/lib/actions/image.actions";
 import { useRouter } from "next/navigation";
 import { updateCredits } from "@/lib/actions/user.actions";
+import InsufficientCreditsModal from "./InsufficientCreditsModal";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -206,6 +207,7 @@ const TransformationForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField
           control={form.control}
           name="title"
