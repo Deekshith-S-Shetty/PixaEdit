016bcc57bc6510b8bcda7582f80e839c30d75ebc
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-08-26 21:13:26 +0530

    install pagination and setup home page

---
+++ src/components/shared/Collection.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import Search from "./Search";
+import Card from "./Card";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { Button } from "../ui/button";
+
+type CollectionProps = {
+  hasSearch: boolean;
+  images: any;
+  totalPages?: number;
+  page: number;
+};
+
+const Collection = ({
+  hasSearch,
+  images,
+  totalPages = 1,
+  page,
+}: CollectionProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const onPageChange = (action: string) => {
+    const pageValue = action === "next" ? Number(page) + 1 : Number(page) - 1;
+
+    const newUrl = formUrlQuery({
+      searchParams: searchParams.toString(),
+      key: "page",
+      value: pageValue,
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
+
+  return (
+    <>
+      <div className="collection-heading">
+        <h2 className="h2-bold text-dark-600">Recent Edits</h2>
+        {hasSearch && <Search />}
+      </div>
+
+      {images.length > 0 ? (
+        <ul className="collectioin-list">
+          {images.map((image: any) => (
+            <Card key={image._id} image={image} />
+          ))}
+        </ul>
+      ) : (
+        <div className="collection-empty">
+          <p className="p-20-semibold">Empty List</p>
+        </div>
+      )}
+
+      {totalPages > 1 && (
+        <Pagination className="mt-10">
+          <PaginationContent className="flex w-full">
+            <Button
+              disabled={Number(page) <= 1}
+              className="collection-btn"
+              onClick={() => onPageChange("prev")}
+            >
+              <PaginationPrevious className="hover:bg-transparent hover:text-white" />
+            </Button>
+
+            <p className="flex-center p-16-medium w-fit flex-1">
+              {page} / {totalPages}
+            </p>
+
+            <Button
+              className="button w-32 bg-purple-gradient bg-cover text-white"
+              onClick={() => onPageChange("next")}
+              disabled={Number(page) >= totalPages}
+            >
+              <PaginationNext className="hover:bg-transparent hover:text-white" />
+            </Button>
+          </PaginationContent>
+        </Pagination>
+      )}
+    </>
+  );
+};
+
+export default Collection;
